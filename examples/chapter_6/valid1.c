b8367633b195241a7b35cf35058147300866227d
Adriano Leão <adrianocoelho133@gmail.com>
2025-09-18 13:19:42 -0300

    feat(examples): add in the new sixth chapter examples

---
+++ examples/chapter_6/valid1.c
@@ -0,0 +1,7 @@
+int main(void) {
+  int a = 5;
+  if (a > 3)
+    return a;
+  else
+    return 0;
+}
---
+++ examples/chapter_6/valid2.c
@@ -0,0 +1,10 @@
+int main(void) {
+  int a = 2;
+  if (a)
+    if (a > 1)
+      return 10;
+    else
+      return 5;
+  else
+    return 1;
+}
